c409589b748a1d316045749632fd4ac978e56a69
Biraj Mahanta <birajj2001@gmail.com>
2026-04-02 16:42:14 +0530

    Commit: to rebuild github CI

--- config/plugins.ts
+++ config/plugins.ts
@@ -2,33 +2,33 @@ export default ({ env }) => ({
   documentation: {
     enabled: true,
     config: {
-      openapi: '3.0.0',
+      openapi: "3.0.0",
       info: {
-        version: '1.0.0',
-        title: 'Portfolio API',
-        description: 'Portfolio backend API documentation',
+        version: "1.0.0",
+        title: "Portfolio API",
+        description: "PersonalPortfolio backend API documentation",
         contact: {
-          name: 'API Support',
+          name: "API Support",
         },
       },
-      'x-strapi-config': {
-        plugins: ['users-permissions'],
-        path: '/documentation',
+      "x-strapi-config": {
+        plugins: ["users-permissions"],
+        path: "/documentation",
       },
       security: [{ bearerAuth: [] }],
       securitySchemes: {
         bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT',
+          type: "http",
+          scheme: "bearer",
+          bearerFormat: "JWT",
         },
       },
     },
   },
-  'users-permissions': {
+  "users-permissions": {
     config: {
       jwt: {
-        expiresIn: env('JWT_EXPIRES_IN', '7d'),
+        expiresIn: env("JWT_EXPIRES_IN", "7d"),
       },
     },
   },
